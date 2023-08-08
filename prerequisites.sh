be5d9a28959d736b3a6e5e43fd3fdf2257d79dc3
Tong Wang <watong@microsoft.com>
2023-08-08 09:48:04 +0000

    modify dataset path in script

--- prerequisites.sh
+++ prerequisites.sh
@@ -5,10 +5,8 @@
 
 set -e
 
-wget https://msracb.blob.core.windows.net/pub/igt-ckpt.zip
 unzip igt-ckpt.zip
 rm igt-ckpt.zip
 
-wget https://msracb.blob.core.windows.net/pub/igt-data.zip
 unzip igt-data.zip
 rm igt-data.zip
